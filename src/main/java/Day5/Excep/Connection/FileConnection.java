42f54c62745990412136e79fc23165c965f9c969
adriane <adriane@o4.pl>
2022-07-16 18:25:14 +0200

    update

--- src/main/java/Day5/Excep/Connection/FileConnection.java
+++ src/main/java/Day5/Excep/Connection/FileConnection.java
@@ -1,18 +1,36 @@
 package Day5.Excep.Connection;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class FileConnection {
 
     private boolean connected = false;
 
-    public void connect() throws FileConnectionException{ // wyrzucamy go do góry, obowi¹zek spada na klasê connection
+    public void connect() throws FileConnectionException { // wyrzucamy go do góry, obowi¹zek spada na klasê connection
         Random random = new Random();
         double randomPercent = random.nextDouble(); // 0.0 -> 1.0         ---- np. wartoœc 0.2444455 = 24 %
         if (randomPercent < 0.25) {
             throw new FileConnectionException(); // wyj¹tek trzeba obs³u¿yæ bo dziedziczy po Excpetion
         }
         connected = true;
+    }
+
+    public List<String> getBooksFromFile() {
+        File file = new File("src\\Main\\java\\Day5\\Excep\\Wywolanie\\ksiazki2.txt");
+        try {
+            FileReader fileReader = new FileReader(file); // obiekt pomocnizy, / wymaga obs³ugi wyj¹tku
+            BufferedReader bufferedReader = new BufferedReader(fileReader); // wczytywanie pliku tekstowego  z bufferreader ale mozna Scannerem (³atwiej);
+            String line = bufferedReader.readLine();
+            System.out.println(line);
+        } catch (IOException e) {
+            System.out.println("Nie znaleziono pliku!");
+            return new ArrayList<>();
+
+        }
+        return null;
     }
 
     @Override   // to  String po to aby wyswietlac stan fileconnection
--- src/main/java/Day5/Excep/Connection/Main.java
+++ src/main/java/Day5/Excep/Connection/Main.java
@@ -1,14 +1,18 @@
 package Day5.Excep.Connection;
 
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         FileConnection fileConnection = new FileConnection();
         try {
             fileConnection.connect();
+            fileConnection.getBooksFromFile();
         } catch (FileConnectionException e) {
             System.out.println("B³¹d przy po³¹czeniu ");
         }
         System.out.println(fileConnection);
     }
+
 }
