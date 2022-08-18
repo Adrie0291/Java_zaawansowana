6453d2e3d369a760f2b998820064050e68254d48
Adriane0291 <adrianbloch01@gmail.com>
2022-08-18 20:28:27 +0200

    serialization

--- src/main/java/Day19/inputOutput/SerializationDemo.java
+++
@@ -1,9 +0,0 @@
-package Day19.inputOutput;
-
-public class SerializationDemo {
-    public static void main(String[] args) {
-      GameCatalog gameCatalog = new GameCatalog();
-       // gameCatalog.saveAllGames();
-        gameCatalog.readAllGames();
-    }
-}
